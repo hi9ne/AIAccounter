09f3ea2506ffcfd9d42485003b03d2467b52cca4
alik3002 <mukanbetovalibek7@gmail.com>
2025-11-20 00:12:27 +0600

    fix: revert to allow_origins=['*'] as it works

--- backend/app/main.py
+++ backend/app/main.py
@@ -44,14 +44,7 @@ def check_origin(origin: str) -> bool:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173", 
-        "http://localhost:5500",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5500",
-        "https://aiaccounter.pages.dev"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
